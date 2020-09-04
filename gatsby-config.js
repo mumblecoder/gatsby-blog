ca2c38b752f299a1bf73a5c6677c2067d1f129f7
mumblecoder <61308646+mumblecoder@users.noreply.github.com>
2020-09-04 10:06:00 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -187,7 +187,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/logo.png'
+        icon: 'static/computer.png'
       },
     },
     'gatsby-plugin-offline',
