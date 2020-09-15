31e670a55104439e278f1147c666af9728fc93e2
mumblecoder <61308646+mumblecoder@users.noreply.github.com>
2020-09-15 11:02:49 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
       },
