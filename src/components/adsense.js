7a2594722fcaedf845fcfa84287315f1f1171d93
mumblecoder <61308646+mumblecoder@users.noreply.github.com>
2020-10-18 00:12:18 +0900

    Update adsense.js

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+
 function Adsense() {
   const { site } = useStaticQuery(
     graphql`
@@ -15,19 +16,19 @@ function Adsense() {
       }
     `
   );
-    
-  useEffect(()=>{
+
+  useEffect(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});   
-  })
-    return(
-        <div style={{padding: 8}}>
-            <ins class='adsbygoogle'
-                style={ { display: 'block' } }
-                data-ad-client={ site.siteMetadata.adsense.adClient }
-                data-ad-slot={ site.siteMetadata.adsense.adSlot }
-                data-ad-format='auto'
-                data-full-width-responsive='true'></ins>
-        </div>
-    )
+  });
+  return (
+      <div style={{ padding: 8 }}>
+          <ins class='adsbygoogle'
+              style={ { display: 'block' } }
+              data-ad-client={ site.siteMetadata.adsense.adClient }
+              data-ad-slot={ site.siteMetadata.adsense.adSlot }
+              data-ad-format='auto'
+              data-full-width-responsive='true'></ins>
+      </div>
+  );
 }
 export default Adsense;
